6dfcc7178b48da9416fc9511b38951ced5c844c4
hammahutz <62766637+hammahutz@users.noreply.github.com>
2023-01-21 18:59:24 +0100

    277

---
+++ MonoGameMastery.VerticalShooter/Levels/LevelEvents.cs
@@ -0,0 +1,27 @@
+using MonoGameMastery.GameEngine.States;
+
+namespace MonoGameMastery.VerticalShooter.Levels;
+
+public class LevelEvents : BaseGameStateEvent
+{
+    public class GenerateEnemies : LevelEvents
+    {
+        public int NbEnemies { get; private set; }
+        public GenerateEnemies(int nbEnemies)
+        {
+            NbEnemies = nbEnemies;
+        }
+
+    }
+
+    public class GenderateTurret : LevelEvents
+    {
+        public int XPosition { get; private set; }
+        public GenderateTurret(int xPosition) => XPosition = xPosition;
+    }
+
+    public class StartLevel : LevelEvents { }
+    public class EndLevel : LevelEvents { }
+    public class NoRow { : LevelEvents { }
+
+}
